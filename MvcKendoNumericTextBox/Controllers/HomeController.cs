41bb21d29b0f87a0007ae97d7b98636b21d45d83
Dan Lewis <daniellewis@outlook.fr>
2015-09-16 10:11:43 +0200

    Working solution of Kendo Numeric TextBox when posting back to an MVC controller

---
+++ MvcKendoNumericTextBox/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using MvcKendoNumericTextBox.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKendoNumericTextBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly List<Person> people = new List<Person>
+        {
+            new Person
+            {
+                Id  = 1,
+                Name = "John",
+                Age = 22M
+            },
+            new Person
+            {
+                Id  = 2,
+                Name = "George",
+                Age = 20M
+            },
+            new Person
+            {
+                Id  = 3,
+                Name = "Paul",
+                Age = 23M
+            },
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(people);
+        }
+
+        // GET: Home
+        public ActionResult Create()
+        {
+            var model = new Person();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                model.Id = people.Max(m => m.Id) + 1;
+                people.Add(model);
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
